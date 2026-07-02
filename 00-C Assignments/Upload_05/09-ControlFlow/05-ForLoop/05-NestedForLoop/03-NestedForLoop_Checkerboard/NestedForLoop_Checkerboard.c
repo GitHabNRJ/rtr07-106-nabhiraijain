3bdfea3d4d8e8bf6606ee275d1a0622dc0e6e288
GitHabNRJ <nabhiraijain@gmail.com>
2026-07-02 16:58:14 +0530

    with changes

--- 00-C Assignments/Upload_05/09-ControlFlow/05-ForLoop/05-NestedForLoop/03-NestedForLoop_Checkerboard/NestedForLoop_Checkerboard.c
+++ 00-C Assignments/Upload_05/09-ControlFlow/05-ForLoop/05-NestedForLoop/03-NestedForLoop_Checkerboard/NestedForLoop_Checkerboard.c
@@ -5,7 +5,7 @@ int main(void)
 	int nrj_i, nrj_j, nrj_c;
 
 	//code
-	printf("/n/n");
+	printf("\n\n");
 	for (nrj_i = 0; nrj_i < 64; nrj_i++)
 	{
 		for (nrj_j = 0; nrj_j < 64; nrj_j++)
@@ -16,9 +16,9 @@ int main(void)
 
 			nrj_c = ((nrj_i & 0x8) == 0) ^ ((nrj_j & 0x8) == 0);  
 			if (nrj_c == 0)
-				printf(" ");
+				printf("  ");
 			if (nrj_c == 1)
-				printf("*");
+				printf("* ");
 
          }
 		printf("\n\n");
